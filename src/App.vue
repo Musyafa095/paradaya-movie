95f40104d6b28b8eb9117b3fd6ebb3158d790477
Musyafa095 <musyafa095@gmail.com>
2025-02-14 23:27:15 +0700

    init: initialize Vue 3 project with Vite & Tailwind

---
+++ src/App.vue
@@ -0,0 +1,8 @@
+<template>
+<h1 class="text-3xl font-bold
+ underline text-center text-sky-500">Hello world!</h1>
+ <button class="btn btn-primary">Tombol DaisyUI</button>
+
+ </template>
+
+<script setup></script>
